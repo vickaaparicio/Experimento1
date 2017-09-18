923dd3c907edec1ac6d7696cac4925777640e650
Vickaaparicio <vickaaparicio@gmail.com>
2017-09-18 09:18:42 -0500

    skelton

---
+++ skeleton/footer.php
@@ -0,0 +1,38 @@
+<section id="footer">
+
+<div>
+	<ul class="columna1">
+		<li>
+		<a href="">INICIO</a>
+		</li>
+
+		<li>
+		<a href="">VENTAS</a>
+		</li>
+
+	</ul>
+	
+	<ul>
+		<li>
+		<a href="">RENTAS</a>
+		</li>
+
+		<li>
+		<a href="">CONTACTO</a>
+		</li>
+		
+	</ul>
+
+	<ul class="columna2">
+		<li>
+		<a href="">271 125 2233</a>
+		</li>
+
+		<li>
+		<a href="">INMOBILIARIA VICKA</a>
+		</li>
+		
+	</ul>
+</div>
+<p>www.inmovicka.com</p>
+</section>
---
+++ skeleton/gallery.php
@@ -0,0 +1,19 @@
+<section id="gallery">
+
+<div>
+	<ul>
+		<li>
+			<img src="img/casa1.jpeg">
+		</li>
+
+		<li>
+			<img src="img/casa2.jpeg">
+		</li>
+
+		<li>
+			<img src="img/casa3.jpg">
+		</li>
+	</ul>
+</div>
+	
+</section>
